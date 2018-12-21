6b5c22d0ea3e6b9bba23952f0b0fe5c80456ec24
Andrew Ding <ading25@gmail.com>
2018-12-21 00:09:17 -0500

    Remove create-react-app

--- services/arena-ui/src/index.js
+++ services/arena-ui/src/index.js
@@ -7,7 +7,6 @@ import configureStore from './store';
 
 import './styles/index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <Provider store={configureStore()}>
@@ -16,7 +15,3 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
